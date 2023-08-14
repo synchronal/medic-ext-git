d99740ded361ebe79b5d4a27c49fc1eee5c22818
Eric Saxby <sax@livinginthepast.org>
2023-08-14 16:30:15 -0700

    Fix link-to-actions to handle different remote formats

---
+++ CHANGELOG.md
@@ -0,0 +1,10 @@
+# Change log
+
+## Unrelease
+
+- Fix `medic-step-github link-to-actions` to handle different formats of remotes.
+
+## v0.2.1
+
+- Initial release as separate repo.
+- Includes `medic-step-git` and `medic-step-github`.
--- medic-step-github/src/lib.rs
+++ medic-step-github/src/lib.rs
@@ -18,12 +18,7 @@ pub fn link_to_actions(args: GithubActionsArgs) -> StepResult {
             if cmd.status.success() {
                 let mut origin = std_to_string(cmd.stdout);
                 origin = origin.trim().to_owned();
-                let origin_re = Regex::new(r"(git@|https://)([^:]+)[:/](.+)(?:\.git)$").unwrap();
-                let caps = origin_re.captures(&origin).unwrap();
-                let url = caps.get(2).unwrap().as_str();
-                let repository = caps.get(3).unwrap().as_str();
-
-                let github_url = format!("https://{}/{}", url, repository);
+                let github_url = origin_to_github_url(origin);
 
                 println!("\x1b[93mCheck CI at \x1b[0;1m{github_url}/actions\x1b[0m");
 
@@ -40,5 +35,56 @@ pub fn link_to_actions(args: GithubActionsArgs) -> StepResult {
             None,
             None,
         ),
+    }
+}
+
+pub fn origin_to_github_url(origin: String) -> String {
+    let trailing_git_re = Regex::new(r"\.git$").unwrap();
+    let origin = trailing_git_re.replace_all(&origin, "");
+
+    let origin_re = Regex::new(r"^([^@]+@|[^:]+://)([^:]+)[:/](.+)$").unwrap();
+    let caps = origin_re.captures(&origin).unwrap();
+    let url = caps.get(2).unwrap().as_str();
+    let repository = caps.get(3).unwrap().as_str();
+
+    format!("https://{}/{}", url, repository)
+}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn github_actions_url_from_ssh() {
+        let url = super::origin_to_github_url("git@github.com:my-org/my-repo".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
+    }
+
+    #[test]
+    fn github_actions_url_from_ssh_with_trailing_git() {
+        let url = super::origin_to_github_url("git@github.com:my-org/my-repo.git".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
+    }
+
+    #[test]
+    fn github_actions_url_from_https() {
+        let url = super::origin_to_github_url("https://github.com/my-org/my-repo".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
+    }
+
+    #[test]
+    fn github_actions_url_from_https_with_trailing_git() {
+        let url = super::origin_to_github_url("https://github.com/my-org/my-repo.git".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
+    }
+
+    #[test]
+    fn github_actions_url_from_git() {
+        let url = super::origin_to_github_url("git://github.com/my-org/my-repo".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
+    }
+
+    #[test]
+    fn github_actions_url_from_git_with_trailing_git() {
+        let url = super::origin_to_github_url("git://github.com/my-org/my-repo.git".to_owned());
+        assert_eq!(url, "https://github.com/my-org/my-repo")
     }
 }
